e29c8e1d72ec0c02c9011bddaf60de8958a95e5a
NdiimuZiickey <zeeisa518@gmail.com>
2024-01-20 14:20:01 -0800

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# Creates a file in tmp
+# Create a file in tmp
 #
 # file { '/tmp/school':
-#   ensure  => file,
-#     path    => '/tmp/school',
-#       mode    => '0744',
-#         owner   => 'www-data',
-#           group   => 'www-data',
-#             content => 'I love Puppet'
-#             }
+# ensure => file,
+# path => '/tmp/school',
+# mode => 'www-data',
+# group => 'www-data',
+# content => 'I love Puppet'
+# }
