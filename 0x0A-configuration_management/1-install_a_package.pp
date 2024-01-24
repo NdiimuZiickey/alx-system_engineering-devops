add92bc86dcac0ad318f1235735af680d2519f2a
NdiimuZiickey <zeeisa518@gmail.com>
2024-01-24 15:40:54 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs flask, version 2.1.0.
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+#!/usr/bin/pup
+# Install an specific version of flask(2.1.0)
+package {'flask':
+	ensure => '2.1.0',
+	provider => 'pip3',
 }
